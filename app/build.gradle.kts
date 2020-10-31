b63c3ebd22d81bf7e6b90fb6bd761552a75dc247
s1180031 <65984749+s1180031@users.noreply.github.com>
2020-10-31 13:11:39 +0900

    ktlint導入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,6 @@ android {
 }
 
 dependencies {
-
     implementation(fileTree("dir" to "libs", "include" to listOf("*.jar")))
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
--- app/src/main/java/com/github/s1180031/newfeatureapp/MainActivity.kt
+++ app/src/main/java/com/github/s1180031/newfeatureapp/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.github.s1180031.newfeatureapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
